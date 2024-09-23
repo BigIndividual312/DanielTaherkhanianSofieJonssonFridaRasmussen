39b113d288c0d79418a9705f028c9b7c08926b69
FridaGrutzky <frida.grutzky@gmail.com>
2024-09-23 21:20:58 +0200

    KrakenSetup
    Rörelse för kraken:
    
    -animation som updaterar till slå från gå och från "intro" till gå.
    (vi kan byta sprites sen gjorde snabbt för test)
    
    -script för kraken att han följer efter player
    
    -kill-collider på svansen
    
    -ny bakrund

--- Assets/Scripts/KrakenMovement.cs
+++ Assets/Scripts/KrakenMovement.cs
@@ -1,81 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class KrakenMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 2.0f;
-    [SerializeField] private float bounciness = 100;
-    [SerializeField] private float knockbackForce = 200f;
-    [SerializeField] private float upwardForce = 100f;
     [SerializeField] private int damageGiven = 1;
-    private SpriteRenderer rend;
-    private bool canMove = true;
+    /*  [SerializeField] private float moveSpeed = 2.0f;
+      [SerializeField] private float bounciness = 100;
+      [SerializeField] private float knockbackForce = 200f;
+      [SerializeField] private float upwardForce = 100f;
+      [SerializeField] private int damageGiven = 1;
+      private SpriteRenderer rend;
+      private bool canMove = true;
 
-    private void Start()
+      private void Start()
+      {
+          rend = GetComponent<SpriteRenderer>();
+      }
+      void FixedUpdate()
+      {
+          if (!canMove)
+          {
+              return;
+          }
+
+          transform.Translate(new Vector2(moveSpeed, 0) * Time.deltaTime);
+
+          if (moveSpeed > 0)
+          {
+              rend.flipX = true;
+          }
+
+          if (moveSpeed < 0)
+          {
+              rend.flipX = false;
+          }
+      }
+
+      private void OnCollisionEnter2D(Collision2D other)
+      {
+          if (other.gameObject.CompareTag("EnemyBlock"))
+          {
+              moveSpeed = -moveSpeed;
+          }
+
+          if (other.gameObject.CompareTag("Enemy"))
+          {
+              moveSpeed = -moveSpeed;
+          }
+
+          if (other.gameObject.CompareTag("Player"))
+          {
+              other.gameObject.GetComponent<PlayerMovement>().TakeDamage(damageGiven);
+
+              if (other.transform.position.x > transform.position.x)
+              {
+                  other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(knockbackForce, upwardForce);
+              }
+              else
+              {
+                  other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(-knockbackForce, upwardForce);
+              }
+          }
+      }
+
+    */
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        rend = GetComponent<SpriteRenderer>();
-    }
-    void FixedUpdate()
-    {
-        if (!canMove)
+        if (other.gameObject.CompareTag("Player"))
         {
-            return;
-        }
+            other.gameObject.GetComponent<PlayerMovement>().TakeDamage(damageGiven);
 
-        transform.Translate(new Vector2(moveSpeed, 0) * Time.deltaTime);
-
-        if (moveSpeed > 0)
-        {
-            rend.flipX = true;
-        }
-
-        if (moveSpeed < 0)
-        {
-            rend.flipX = false;
+            if (other.CompareTag("Player"))
+            {
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, 0);
+                //   other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounciness));
+                GetComponent<Animator>().SetTrigger("Hit");
+                GetComponent<BoxCollider2D>().enabled = false;
+                GetComponent<CircleCollider2D>().enabled = false;
+                GetComponent<Rigidbody2D>().gravityScale = 0;
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                //canMove = false;
+                Destroy(gameObject, 0.5f);
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("EnemyBlock"))
-        {
-            moveSpeed = -moveSpeed;
-        }
-
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            moveSpeed = -moveSpeed;
-        }
-
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerMovement>().TakeDamage(damageGiven);
-
-            if (other.transform.position.x > transform.position.x)
-            {
-                other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(knockbackForce, upwardForce);
-            }
-            else
-            {
-                other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(-knockbackForce, upwardForce);
-            }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, 0);
-            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounciness));
-            GetComponent<Animator>().SetTrigger("Hit");
-            GetComponent<BoxCollider2D>().enabled = false;
-            GetComponent<CircleCollider2D>().enabled = false;
-            GetComponent<Rigidbody2D>().gravityScale = 0;
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            canMove = false;
-            Destroy(gameObject, 0.5f);
         }
     }
 }
+
