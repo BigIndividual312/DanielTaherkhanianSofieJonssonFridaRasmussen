00acebb5e2e78bf4add3595612cb55450c2b1a6b
FridaGrutzky <frida.grutzky@gmail.com>
2024-09-25 21:17:45 +0200

    Snyggat till boss animationen
    förkortade boss animationen och la till en hemlig crab

--- Assets/Scripts/KrakenMovement.cs
+++ Assets/Scripts/KrakenMovement.cs
@@ -10,65 +10,7 @@ public class KrakenMovement : MonoBehaviour
     [SerializeField] private float bounciness = 300;
     [SerializeField] private float knockbackForce = 200f;
     [SerializeField] private float upwardForce = 100f;
-    /*  [SerializeField] private int damageGiven = 1;
-      private SpriteRenderer rend;
-      private bool canMove = true;
-
-      private void Start()
-      {
-          rend = GetComponent<SpriteRenderer>();
-      }
-      void FixedUpdate()
-      {
-          if (!canMove)
-          {
-              return;
-          }
-
-          transform.Translate(new Vector2(moveSpeed, 0) * Time.deltaTime);
-
-          if (moveSpeed > 0)
-          {
-              rend.flipX = true;
-          }
-
-          if (moveSpeed < 0)
-          {
-              rend.flipX = false;
-          }
-      }
-
-      private void OnCollisionEnter2D(Collision2D other)
-      {
-          if (other.gameObject.CompareTag("EnemyBlock"))
-          {
-              moveSpeed = -moveSpeed;
-          }
-
-          if (other.gameObject.CompareTag("Enemy"))
-          {
-              moveSpeed = -moveSpeed;
-          }
-
-          if (other.gameObject.CompareTag("Player"))
-          {
-              other.gameObject.GetComponent<PlayerMovement>().TakeDamage(damageGiven);
-
-              if (other.transform.position.x > transform.position.x)
-              {
-                  other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(knockbackForce, upwardForce);
-              }
-              else
-              {
-                  other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(-knockbackForce, upwardForce);
-              }
-          }
-      }
-
-    */
-
-
-    private void OnCollisionEnter2D(Collision2D other)
+       private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -101,14 +43,20 @@ public class KrakenMovement : MonoBehaviour
                 other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, 0);
                 other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounciness));
                 GetComponent<Animator>().SetTrigger("Hit");
-                GetComponent<CircleCollider2D>().enabled = false;
                 GetComponent<BoxCollider2D>().enabled = false;
+                GetComponent<CircleCollider2D>().enabled = false;
                 GetComponent<Rigidbody2D>().gravityScale = 0;
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 //canMove = false;
-                Destroy(gameObject, 8f);
+                Destroy(gameObject, 5f);
+         
             }
         }
     }
-}
+                
+                
+            }
+        
+    
+
 
