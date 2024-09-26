a3a12c5d7031297e961472378ee976650bb951f1
BigIndividual312 <danielt@ownit.nu>
2024-09-26 12:04:02 +0200

    Fixed the rat platform and more
    . Added code to the platform script that allows the rat sprite to turn on the x axis
    
    . Adjusted the position of one of the enemy blocks to no longer be stuck in terrain in scene 2

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -6,6 +6,7 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Transform target1, target2;
     [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private SpriteRenderer ratSpriteRenderer;
 
     private Transform currentTarget;
     private SpriteRenderer rend;
@@ -26,14 +27,28 @@ public class MovingPlatform : MonoBehaviour
         if(transform.position == target1.position)
         {
             currentTarget = target2;
+            FlipRatSprite(false);
         }
 
         if (transform.position == target2.position)
         {
             currentTarget = target1;
+            FlipRatSprite(true);
         }
 
         transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, moveSpeed * Time.deltaTime);
+    }
+
+    private void FlipRatSprite(bool facingLeft)
+    {
+        if(facingLeft)
+        {
+            ratSpriteRenderer.flipX = false;
+        }
+        else
+        {
+            ratSpriteRenderer.flipX = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
